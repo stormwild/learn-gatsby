c9a9b87260988491cf9b25356b9b2e06e39449bf
stormwild <artorrijos@gmail.com>
2021-01-19 06:28:52 +0000

    Updates

---
+++ hello-world/src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default ({ title }) => {
+  return (
+    <div>
+      <h1>Header</h1>
+      <h2>Page Title: {title}</h2>
+    </div>
+  )
+}
---
+++ hello-world/src/pages/about.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => {
+  return (
+    <div>
+      <Header title="About" />
+      <h1>About page</h1>
+    </div>
+  )
+}
